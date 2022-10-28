28a822afdae3ed082393d91be1b46e1e38dae7a4
Lukas Urbonas <prefixltu@gmail.com>
2022-10-28 17:17:01 +0300

    impostor image test build

---
+++ impostor/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/container
+sleep 1
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
